1f91e7412d4af1e4ed026d88f95d62fce88d33a9
cesar <cesarpazol1029@gmail.com>
2023-07-16 19:43:12 -0500

    Anotacion @Repository en Mensaje_Repositorio

--- src/main/java/com/cesar/ChatWeb/repository/Mensaje_Repositorio.java
+++ src/main/java/com/cesar/ChatWeb/repository/Mensaje_Repositorio.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.cesar.ChatWeb.entity.Mensaje;
 
+@Repository
 public interface Mensaje_Repositorio extends JpaRepository<Mensaje, Long> {
 
 	@Query("SELECT m FROM Mensaje m WHERE (m.id_remitente = :remitente OR m.id_remitente = :destinatario) AND (m.id_destinatario = :destinatario OR m.id_destinatario = :remitente) ORDER BY m.fecha ASC")
