c35716daea95cd0c4d44a189d99002b64101f4e4
cesar <cesarpazol1029@gmail.com>
2023-07-22 18:17:39 -0500

    Ruta corregida para PresentacionLogin: faltaba extension jpg

--- src/main/java/com/cesar/ChatWeb/validation/Validador_EmailNoDisponible.java
+++ src/main/java/com/cesar/ChatWeb/validation/Validador_EmailNoDisponible.java
@@ -1,6 +1,15 @@
 package com.cesar.ChatWeb.validation;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 
 import com.cesar.ChatWeb.entity.Usuario;
 import com.cesar.ChatWeb.repository.Usuario_Repositorio;
@@ -11,7 +20,6 @@ import jakarta.validation.ConstraintValidatorContext;
 public class Validador_EmailNoDisponible implements ConstraintValidator<EmailNoDisponible, String> {
 
 	
-	
 	@Override
 	public boolean isValid(String email, ConstraintValidatorContext context) {
 
@@ -19,8 +27,6 @@ public class Validador_EmailNoDisponible implements ConstraintValidator<EmailNoD
 		
 		return u == null ? true : false;
 	}
-	
-	
 	
 	@Autowired
 	private Usuario_Repositorio userRepo;
