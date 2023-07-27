f55d5e6b51fc8d3505193f7309d694741f7b9ab9
cesar <cesarpazol1029@gmail.com>
2023-07-27 14:50:24 -0500

    Cliente: si imagen perfil es muy grande, se desabilita btnRegistrar

--- src/main/resources/static/Js/Cliente_ValidacionRegistro.js
+++ src/main/resources/static/Js/Cliente_ValidacionRegistro.js
@@ -1,1 +1,28 @@
-console.log("---------------")
+$(document).ready(function(){
+	
+	console.log("Documento listo")
+	
+	$("#campoMetadatosImagen").on("input", function(){
+		
+		var metadatosImagen = $(this)[0];
+		
+		if ( metadatosImagen.files.length > 0 ){
+			
+			if ( metadatosImagen.files[0].size > (1*1024*1024) ){
+				
+				$("#btnRegistrar").prop("disabled", true);
+			}
+			else{
+				
+				$("#btnRegistrar").prop("disabled", false);
+			}
+		}
+		else{
+			
+			$("#btnRegistrar").prop("disabled", false);
+		}
+		
+		
+
+	});
+});
