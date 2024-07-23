c041f24bb8a8cdb124004ae89b3219088c02c5cc
worker <worker@core>
2024-07-23 17:39:47 -0600

    Use H2 database for tests instead of Mysql

--- src/main/java/com/cesar/ChatWeb/websocket/Configuracion_WebSocket.java
+++ src/main/java/com/cesar/ChatWeb/websocket/Configuracion_WebSocket.java
@@ -1,8 +1,6 @@
 package com.cesar.ChatWeb.websocket;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
